40b223c9b4178d21467ff712a5b69aca13d9282d
0MazaHacka0 <killerinshadow2@gmail.com>
2017-07-01 19:46:24 +0300

    Edit tasks migration

--- database/migrations/2017_07_01_161412_create_tasks_table.php
+++ database/migrations/2017_07_01_161412_create_tasks_table.php
@@ -15,6 +15,17 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('task_name');
+            $table->bigInteger('user_id')->default('0');
+            $table->string('essid');
+            $table->macAddress('station_mac');
+            $table->integer('type');
+            $table->binary('task_hash');
+            $table->binary('uniq_hash');
+            $table->string('net_key')->nullable();
+            $table->integer('priority')->default('10');
+            $table->integer('agents')->default('0');
+            $table->integer('status')->default('0');
             $table->timestamps();
         });
     }
